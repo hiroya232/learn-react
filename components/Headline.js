91c91b100e9152371b15ee4f9cfe7344b5c7f2ef
hiroya-akeoka <mowoarm232@gmail.com>
2022-11-18 07:46:29 +0900

    :recycle:スタイルファイルの分割

--- components/Headline.js
+++ components/Headline.js
@@ -1,11 +1,11 @@
-import styles from "../styles/Home.module.css";
+import classes from "./Headline.module.css";
 
 export function Headline(props) {
   return (
     <div>
-      <h1 className={styles.title}>{props.page} Page</h1>
+      <h1 className={classes.title}>{props.page} Page</h1>
 
-      <p className={styles.description}>Get started by editing {props.children}</p>
+      <p className={classes.description}>Get started by editing {props.children}</p>
     </div>
   );
 }
