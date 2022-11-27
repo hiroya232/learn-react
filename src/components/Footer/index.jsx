d14f94b29b6c10c80612b7c6f3895dfaa7e1e2b9
hiroya-akeoka <mowoarm232@gmail.com>
2022-11-27 15:25:17 +0900

    🏗️ 拡張子・ディレクトリ構成・インポート方法の変更、_appを用いた処理共通化

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import classes from "./Footer.module.css";
+import classes from "src/components/Footer/Footer.module.css";
 
 export function Footer() {
   return (
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,6 @@
-import classes from "./Main.module.css";
-import { Links } from "../components/Links";
-import { Headline } from "../components/Headline";
+import classes from "src/components/Main/Main.module.css";
+import { Headline } from "src/components/Headline";
+import { Links } from "src/components/Links";
 
 export function Main(props) {
   return (
