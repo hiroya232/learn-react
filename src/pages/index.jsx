d92e78a68e277e0ca4139d428cc14b166ea3c473
hiroya-akeoka <mowoarm232@gmail.com>
2022-12-03 08:35:38 +0900

    ✨講座用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
   const foo = 1;
@@ -12,6 +12,16 @@ export default function Home() {
     console.log(e.target.href);
     e.preventDefault();
     alert(foo);
+  }, []);
+
+  useEffect(() => {
+    console.log("mount");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("unmount");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
